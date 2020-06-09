1945abf82300dce2fab53c412a6940ea43b64721
Trevor Cheche <twitcherfrost@gmail.com>
2020-06-09 19:56:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,10 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def method_display
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+method_display
+  # Define a method display_board that prints a 3x3 Tic Tac Toe Board
